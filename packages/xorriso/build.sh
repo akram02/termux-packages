d91ba07303d862cba6f9dc346af22f5228db6388
Akram Khan <akrammiru@gmail.com>
2024-10-29 23:58:35 +0600

    fix: wrong checksum for xorriso

--- packages/xorriso/build.sh
+++ packages/xorriso/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/xorriso
 TERMUX_PKG_DESCRIPTION="Tool for creating ISO files"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1:1.5.7"
+TERMUX_PKG_VERSION="1.5.7"
 TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://www.gnu.org/software/xorriso/xorriso-${TERMUX_PKG_VERSION:2}.tar.gz
-TERMUX_PKG_SHA256=e9c5b90fae744ab53381fb25d9c96b254c73b387e6252f5da28dd2ede2fb81f6
+TERMUX_PKG_SRCURL=https://www.gnu.org/software/xorriso/xorriso-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0d9cf80df436a16b4c4051fb4893307d109f2394c58fa94138a2270160eaca4d
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libiconv, libandroid-support, readline, libbz2, zlib"
+TERMUX_PKG_DEPENDS="readline, libbz2, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-jtethreads"
 
